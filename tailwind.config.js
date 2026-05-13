e541ff182148b4598e437a2af89d5b0ae30dcfcf
candra2012 <60005380+candra2012@users.noreply.github.com>
2026-05-13 10:47:41 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    typography, // Menambahkan plugin untuk tipografi artikel
+  ],
   darkMode: ['class', '.darkmode'],
 }
